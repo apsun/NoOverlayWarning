6630ba75d22e3f1d48303d70ea2d4236603f4f55
Andrew Sun <0xandrewsun@gmail.com>
2016-08-30 06:55:52 -0500

    Support Samsung and some other ROMs

--- src/com/crossbowffs/nooverlaywarning/Hook.java
+++ src/com/crossbowffs/nooverlaywarning/Hook.java
@@ -9,11 +9,22 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 public class Hook implements IXposedHookLoadPackage {
     private static final String TAG = "NoOverlayWarning";
-    private static final String[] INSTALLER_PACKAGE_NAMES = {
-        "com.android.packageinstaller",        // Pre-Marshmallow
-        "com.google.android.packageinstaller", // Marshmallow
-        "com.mokee.packageinstaller"           // MoKee
+
+    // Whether to hook all packages with name ending in ".packageinstaller",
+    // since people enjoy changing the package name for some reason.
+    private static final boolean HOOK_ALL_PACKAGE_INSTALLERS = true;
+
+    // Known package installer package name whitelist, used when
+    // HOOK_ALL_PACKAGE_INSTALLERS is false.
+    private static final String[] KNOWN_PACKAGE_INSTALLERS = {
+        "com.android.packageinstaller",         // Pre-Marshmallow
+        "com.google.android.packageinstaller",  // Marshmallow
+        "com.samsung.android.packageinstaller", // Samsung
+        "com.mokee.packageinstaller"            // MoKee
     };
+
+    // New flag secretly added in Android 6.0.1 it seems
+    // https://android.googlesource.com/platform/frameworks/native/+/03a53d1c7765eeb3af0bc34c3dff02ada1953fbf%5E!/
     private static final int FLAG_WINDOW_IS_PARTIALLY_OBSCURED = 0x2;
 
     private static boolean arrayContains(String[] array, String value) {
@@ -25,9 +36,20 @@ public class Hook implements IXposedHookLoadPackage {
         return false;
     }
 
+    private static boolean shouldHook(String packageName) {
+        if (arrayContains(KNOWN_PACKAGE_INSTALLERS, packageName)) {
+            return true;
+        }
+        if ((packageName.endsWith(".packageinstaller"))) {
+            Log.w(TAG, "Unknown package installer name: " + packageName);
+            return HOOK_ALL_PACKAGE_INSTALLERS;
+        }
+        return false;
+    }
+
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
-        if (!arrayContains(INSTALLER_PACKAGE_NAMES, lpparam.packageName)) {
+        if (!shouldHook(lpparam.packageName)) {
             return;
         }
 
@@ -40,8 +62,6 @@ public class Hook implements IXposedHookLoadPackage {
                     Log.i(TAG, "Cleared FLAG_WINDOW_IS_OBSCURED flag");
                 }
 
-                // New flag secretly added in Android 6.0.1 it seems
-                // https://android.googlesource.com/platform/frameworks/native/+/03a53d1c7765eeb3af0bc34c3dff02ada1953fbf%5E!/
                 if ((flags & FLAG_WINDOW_IS_PARTIALLY_OBSCURED) != 0) {
                     flags &= ~FLAG_WINDOW_IS_PARTIALLY_OBSCURED;
                     Log.i(TAG, "Cleared FLAG_WINDOW_IS_PARTIALLY_OBSCURED flag");
